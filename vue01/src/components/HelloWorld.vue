3ef1a5b8a15e460354d869d3348d279f44b66c3f
lm20181228 <963140336@qq.com>
2019-08-06 17:55:24 +0800

    首页固定栏，element-ui的初步使用。vue+webpack的初步使用。跨出了 第一步

--- vue01/src/components/HelloWorld.vue
+++ vue01/src/components/HelloWorld.vue
@@ -1,7 +1,7 @@
 <template>
   <div class="hello">
     <h1>{{ msg }}</h1>
-    <h2>Essential Links</h2>
+    <h2>00000000000000000000000</h2>
     <ul>
       <li>
         <a
--- vue01/src/components/index.vue
+++ vue01/src/components/index.vue
@@ -1,8 +1,29 @@
 <template>
+	<div >
+		<el-container>
+			<el-header class="fixedTop fixedPosistion">
+				<el-row :gutter="20">
+					<el-col :span="10" >
+						<div class="grid-content bg-purple">我的</div>
+					</el-col>
+					<el-col :span="10">
+						<div class="grid-content bg-purple">推荐</div>
+					</el-col>
+				</el-row>
+			</el-header>
+			<el-main id="app" class="contentMain">
+				<span>主要区域容器</span>
+			</el-main>
+			<el-footer class="fixedFooter fixedPosistion">
+				<span>底栏容器</span>
+			</el-footer>
+		</el-container>
+	</div>
 </template>
 
 <script>
 </script>
 
 <style>
+
 </style>
