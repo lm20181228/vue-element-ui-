a4ce5e84b0372ed33275623e2bb3938a437b4979
limei <963140336@qq.com>
2019-08-08 01:18:04 +0800

    首页-我的，我的歌单新增歌单功能

--- vue01/src/components/my/myMusic.vue
+++ vue01/src/components/my/myMusic.vue
@@ -31,9 +31,17 @@
 	  	<!--本地音乐E-->
 	  	<!--歌单部分S-->
 	  	<div class="songSheet">
-	  		<el-collapse v-model="activeNames" @change="handleChange">
-	  			<el-collapse-item title="创建的歌单" name = "1">
-	  				<div class="text" v-for="item in songsLists">
+	  		<el-collapse >
+	  			<!--v-model="activeNames" @change="handleChange"-->
+	  			<el-collapse-item   name = "1">
+	  				<div class="collapse-title songSheetTitle" slot="title">我的歌单({{songsLists.length}})
+	  					<div class="operation">
+	  						<span class="addMyList" @click="dialogFormVisible = true">+</span>
+	  						<span class="moreOperations"  @click="drawer = true">:</span>
+	  					</div>
+	  				</div>
+	  				
+	  				<div class="text" v-for="item in songsLists.list">
 				  		<router-link to="/localMusic">
 				  			<el-row :gutter="24">
 				  				<el-col :span="4">
@@ -50,15 +58,20 @@
 				  		</router-link>	
 				  	</div>
 	  			</el-collapse-item>
-	  			<el-collapse-item title="收藏的歌单" name = "2">
-	  				<div class="text ">
+	  			<el-collapse-item name = "2">
+	  				<span class="collapse-title" slot="title">收藏的歌单({{collectionSongsLists.length}})</span>
+	  				<div class="text " v-for="item in collectionSongsLists.lists">
 				  		<router-link to="/localMusic">
 				  			<el-row :gutter="24">
 				  				<el-col :span="4">
-				  					<span class="localInfoIcon">sdf</span>
+				  					<span class="songSheetImg"><img :src="item.img" alt="" /></span>
 				  				</el-col>
-				  				<el-col :span="20">
-				  					<span class="localName">我的收藏</span>
+				  				<el-col :span="16">
+				  					<span class="">{{item.name}}</span>
+				  					<p>{{item.mun}}</p>
+				  				</el-col>
+				  				<el-col :span="4">
+				  					<span class="">+</span>
 				  				</el-col>
 				  			</el-row>
 				  		</router-link>	
@@ -67,7 +80,27 @@
 	  		</el-collapse>
 	  	</div>
 	  	<!--歌单部分E-->
+	  	<!--新增我的歌单弹窗S-->
 	  	
+	  	<!--新增我的歌单弹窗E-->
+		<el-drawer
+		  title="创建的歌单"
+		  :visible.sync="drawer"
+		  :direction="direction"
+		  >
+		  <span>我来啦!</span>
+		</el-drawer>
+		<!--新建歌单信息S-->
+		<el-dialog title="新建歌单" :visible.sync="dialogFormVisible">
+			  <el-form :model="form">
+			    <el-input v-model="form.name" autocomplete="off" ></el-input>
+			  </el-form>
+			  <div slot="footer" class="dialog-footer">
+			    <span @click="cancel()">取 消</span>
+			    <span type="primary"  @click="submit()">提  交</span>
+			  </div>
+		</el-dialog>
+		<!--新建歌单信息E-->
 	</div>
 </template>
 
@@ -134,32 +167,85 @@
 	        }],
 	        activeNames: ['1'],
 	        //创建歌单
-	        songsLists:[{
-	        	id:1,
-	        	name:"歌单1",
-	        	img:"/static/img/wangyiyun.cb53c5d.png",
-	        	mun:12
-	        },{
-	        	id:2,
-	        	name:"歌单2",
-	        	img:"/static/img/wangyiyun.cb53c5d.png",
-	        	mun:1
-	        },{
-	        	id:3,
-	        	name:"歌单3",
-	        	img:"/static/img/wangyiyun.cb53c5d.png",
-	        	mun:30
-	        },{
-	        	id:4,
-	        	name:"歌单4",
-	        	img:"/static/img/wangyiyun.cb53c5d.png",
-	        	mun:24
-	        }]
+	        songsLists:{
+	        	list:[{
+		        	id:0,
+		        	name:"歌单1",
+		        	img:"/static/img/wangyiyun.cb53c5d.png",
+		        	mun:12
+		        },{
+		        	id:1,
+		        	name:"歌单2",
+		        	img:"/static/img/wangyiyun.cb53c5d.png",
+		        	mun:1
+		        },{
+		        	id:2,
+		        	name:"歌单3",
+		        	img:"/static/img/wangyiyun.cb53c5d.png",
+		        	mun:30
+		        },{
+		        	id:3,
+		        	name:"歌单4",
+		        	img:"/static/img/wangyiyun.cb53c5d.png",
+		        	mun:24
+		        }],
+	        	length:4
+	        },
+	        
+	        /*收藏歌单*/
+	       	collectionSongsLists:{
+	       		lists:[{
+		        	id:1,
+		        	name:"收藏歌单1",
+		        	img:"/static/img/wangyiyun.cb53c5d.png",
+		        	mun:30
+		        },{
+		        	id:2,
+		        	name:"收藏歌单2",
+		        	img:"/static/img/wangyiyun.cb53c5d.png",
+		        	mun:24
+		        }],
+		        length:2
+	       	},
+	       	drawer: false,
+        	direction: 'btt',
+        	dialogTableVisible: false,
+        	dialogFormVisible: false,
+        	form: {
+	          name: '',
+	          region: '',
+	          date1: '',
+	          date2: '',
+	          delivery: false,
+	          type: [],
+	          resource: '',
+	          desc: ''
+	        },
+	        formLabelWidth: '120px'
 	      };
 	    },
 	    methods: {
 	      handleChange(val) {
 	        console.log(val);
+	      },
+	      addList(event){
+	      	event.preventDefault();
+	      	console.log("asd")
+	      },
+	      cancel(){
+	      	this.dialogFormVisible=false;
+	      },submit(){
+	      	console.log("提交");
+	      	var newSongs={
+	        	id:this.songsLists.length,
+	        	name:this.form.name,
+	        	img:"/static/img/wangyiyun.cb53c5d.png",
+	        	mun:0
+	       	};
+	       	this.songsLists.list.unshift(newSongs);
+	       	this.songsLists.length++;
+	       	this.form.name="";
+	      	this.dialogFormVisible=false;
 	      }
 	    }
 	  }
@@ -243,4 +329,27 @@
 	.songSheetImg>img{
 		width: 100%;
 	}
+	 .collapse-title {
+        flex: 1 0 90%;
+        order: 1;
+    }
+    .el-collapse-item__header {
+        flex: 1 0 auto;
+        order: -1;
+    }
+    .songSheetTitle{
+    	position: relative;
+    }
+    .operation{
+    	border: 1px solid red;
+    	position: absolute;
+    	top: 0;
+    	right: 0;
+    	width: 40px;
+    }
+    .operation>span{
+    	display: inline-block;
+    	width: 18px;
+    	text-align: center;
+    }
 </style>
