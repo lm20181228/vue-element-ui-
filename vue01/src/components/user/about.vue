0a1fcb0b67f2da1086ecd637980889864fe26c64
lm20181228 <963140336@qq.com>
2019-09-02 16:54:29 +0800

    用户模块-动态，关于我的模块盘版

--- vue01/src/components/user/about.vue
+++ vue01/src/components/user/about.vue
@@ -1,30 +1,37 @@
 <template>
-	<div>
+	<div class="about">
 		<!--个人信息-->
-		<div>
-			<h2>个人信息</h2>
-			<div>
-				<p><span>等级：</span>{{userInfo.level}}</p>
-				<p><span>年龄：</span>{{userInfo.birthday}}</p>
-				<p><span>性别：</span>{{userInfo.sex}}</p>
-				<p><span>地区：</span>{{userInfo.region}}</p>
+		<div class="">
+			<h2 class="aboutTitle">个人信息</h2>
+			<div >
+				<p class="colorGray"><span>等级：</span>{{userInfo.level}}</p>
+				<p class="colorGray"><span>年龄：</span>{{userInfo.birthday}}</p>
+				<p class="colorGray"><span>性别：</span>{{userInfo.sex}}</p>
+				<p class="colorGray"><span>地区：</span>{{userInfo.region}}</p>
 			</div>
 			
 		</div>
 		
 		<!--个人介绍-->
-		<div>
-			<h2>个人介绍</h2>
-			<div>
-				<p>{{userInfo.sign}}</p>
+		<div class="">
+			<h2 class="aboutTitle">个人介绍</h2>
+			<div >
+				<p class="colorGray">{{userInfo.sign}}</p>
 			</div>
 		</div>
 		<!--我的评论-->
 		<div>
-			<h2>我的评论</h2>
-			<!--<div>
-				<p>{{userInfo.sign}}</p>
-			</div>-->
+			<h2 class="aboutTitle">我的评论<span class="colorGray fontSmall">（仅自己可见，可在隐私设置中修改）</span></h2>
+			<div class="userComment" v-for="(item , i) in userComment">
+				<div id="">
+					<span class="commentImg"><img :src="item.target.img" alt="" /></span>
+					<span class="colorGray">{{item.target.name}}-{{item.target.author}}</span>
+				</div>
+				<div class="desc">
+					{{item.comment}}
+				</div>
+				<p><span class="colorGray fontSmall">{{item.createTime}}</span></p>
+			</div>
 		</div>
 	</div>
 </template>
@@ -34,7 +41,8 @@
 	export default{
 		data(){
 			return{
-				userInfo:{}
+				userInfo:{},
+				userComment:{}
 			}
 		},
 		mounted(){
@@ -44,9 +52,49 @@
 				console.log(res.data.user[userId])
 				this.userInfo=res.data.user[userId];
 			})
+			this.$axios.get("/static/json/userComment.json")
+			.then((res)=>{
+				this.userComment=res.data.data;
+			})
+			
 		}
 	}
 </script>
 
 <style>
+	.colorGray,.colorGray *{
+		color: #a5a5a5;
+	}
+	.fontSmall{
+		font-size: 12px;
+	}
+	.about{
+		width: 90%;
+		margin: auto;
+	}
+	.about>div{
+		margin-bottom: 20px;
+	}
+	.aboutTitle{
+		height: 46px;
+		line-height: 46px;
+	}
+	.desc{
+		margin: 10px 0;
+	}
+	.userComment{
+		padding: 20px 0;
+		border-bottom: 1px solid #e4e4e4;
+	}
+	.commentImg{
+		display: inline-block;
+		width: 40px;
+		height: 40px;
+		vertical-align: middle;
+	}
+	.commentImg>img{
+		width: 100%;
+		height: 100%;
+		border-radius: 4px;
+	}
 </style>
--- vue01/src/components/user/dynamic.vue
+++ vue01/src/components/user/dynamic.vue
@@ -1,9 +1,87 @@
 <template>
-	<p>动态动态动态动态动态动态动态动态</p>
+	<div>
+		<!--分享详情-->
+		<div class="dynamic" v-for="(item , i) in shareComment.share">
+			<div class="dynamicLeft">
+				<img :src="item.creatAuthor.headPortrait" alt="" />
+			</div>
+			<div class="dynamicRight">
+				<p class="shareTitle"><span class="colorTag">{{item.creatAuthor.name}}</span><span>{{item.type}}分享单曲:</span></p>
+				<p><span class="creatTime">{{item.creatTime}}</span></p>
+				<div>
+					<p class="shareDesc"><span class="colorTag" v-for="tag in item.topPic">#{{tag}}#</span>{{item.desc}}</p>
+					<div >
+						<audio src=""></audio>
+					</div>
+				</div>
+				<el-row :gutter="24" class="operate">
+					<el-col :span="6"><span>转发 {{item.operation.forward.length}}</span></el-col>
+					<el-col :span="6"><span>留言  {{item.operation.comment.length}}</span></el-col>
+					<el-col :span="6"><span>点赞  {{item.operation.likes.length}}</span></el-col>
+					<el-col :span="6" class="text-right"><span>:</span></el-col>
+				</el-row>
+			</div>
+		</div>
+	</div>
 </template>
 
 <script>
+	export default{
+		data(){
+			return {
+				shareComment:{}
+			}
+		},
+		methods:{
+			
+		},
+		mounted(){
+			this.$axios.get("/static/json/userGynamic.json")
+			.then((res)=>{
+				console.log(res.data);
+				this.shareComment=res.data;
+			})
+		}
+	}
 </script>
 
 <style>
+	*{
+	    box-sizing: border-box;
+	}
+	.dynamic{
+		width: 90%;
+		margin: 0 auto;
+	}
+	.dynamicLeft{
+		display: inline-block;
+		width: 20%;
+		vertical-align: top;
+	}
+	.dynamicLeft>img{
+		width: 80%;
+		text-align: center;
+		border-radius: 50%;
+	}
+	.dynamicRight{
+		display: inline-block;
+		width: 78%;
+	}
+	.operate>div{
+		padding: 0!important;
+		margin: 10px 0;
+	}
+	.colorTag{
+		color: #6485A7;
+	}
+	.shareTitle{
+		color: #666;
+	}
+	.creatTime{
+		font-size: 12px;
+		color: #999
+	}
+	.shareDesc{
+		color: #111;
+	}
 </style>
